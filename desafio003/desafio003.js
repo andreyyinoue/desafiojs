c2f4b048ab2858127d389006cd367ed0161b5baa
André Inoue <118934334+andreyyinoue@users.noreply.github.com>
2023-02-01 17:59:59 -0300

    Desafio 003 de JavaScript

---
+++ desafio003/desafio003.js
@@ -0,0 +1,10 @@
+function calc() {
+    let n1 = document.getElementById('inum1')
+    let n2 = document.getElementById('inum2')
+    let res = document.getElementById('resultado')
+    if (n1.value.length == 0 || n2.value.length == 0) {
+        alert('Dados incompletos. Preencha os campos')
+    } else {
+        res.innerHTML = `A soma entre ${n1.value} e ${n2.value} é igual a${Number(n1.value) + Number(n2.value)}`
+    }
+}
